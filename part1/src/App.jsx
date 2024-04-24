39ab0ab3e02e6ccb3109bea53e41e6b43572df77
Jason Ling <sanicthefastmaster@gmail.com>
2024-04-24 13:27:45 +0800

    Added Part 1D - Complex State, debugging React apps, section Rule of Hooks

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -13,37 +13,59 @@ const Hello = ({name, age}) => {
 
 const Display = ({counter}) => <div>{counter}</div>
 
-const Button = ({onUse, text}) => {
+const Button = ({handleClick, text}) => {
     return (
-        <button onClick={onUse}>
+        <button onClick={handleClick}>
             {text}
         </button>
     )
 }
 
-const App = () => {
-    const [counter, setCounter] = useState(0)
-    console.log('rendering with counter value', counter)
+const History = (props) => {
+    if (props.allClicks.length === 0) {
+        return (
+            <div>
+                THe app is used by pressing the buttons
+            </div>
+        )
+    }
+    return(
+        <div>
+            Button press history: {props.allClicks.join(' ')}
+        </div>
+    )
+}
 
-    const increaseOne = () => {
-        console.log('increasing, value before', counter)
-        setCounter(counter + 1)
+const App = () => {
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
+    const [allClicks, setAll] = useState([])
+    const [total, setTotal] = useState(0)
+
+    const handleLeftClick = () => {
+        setAll(allClicks.concat('L'))
+        const updatedLeft = left + 1
+        setLeft(updatedLeft)
+        setTotal(updatedLeft + right)
     }
-    const resetZero = () => {
-        console.log('resetting value to zero, value before', counter)
-        setCounter(0)
+
+    const handleRightClick = () => {
+        setAll(allClicks.concat('R'))
+        const updatedRight = right + 1
+        setRight(updatedRight)
+        setTotal(left + updatedRight)
     }
-    const decreaseOne = () => {
-        console.log('decreasing, value before', counter)
-        setCounter(counter - 1)
-    }
+
+    debugger
 
     return (
         <div>
-            <Display counter={counter}/>
-            <Button onClick={increaseOne} text='plus'/>
-            <Button onClick={decreaseOne} text='minus' />
-            <Button onClick={resetZero} text='reset' />
+            {left}
+            <Button handleClick={handleLeftClick} text='left'/>
+            <Button handleClick={handleRightClick} text='right'/>
+            {right}
+            <p>{allClicks.join(' ')}</p>
+            <History allClicks={allClicks} />
         </div>
     )
 }
