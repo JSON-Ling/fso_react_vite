5fad2a646128c8a3f16585a77b6b6dd41eda26a3
JSON-Ling <sanicthefastmaster@gmail.com>
2024-05-16 15:38:34 +0800

    Added FSO part 2 section b - Forms

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,11 +1,30 @@
 import Note from "./components/Note.jsx";
+import {useState} from "react";
 
-const App = ({notes}) => {
+const App = (props) => {
+    const [notes, setNotes] = useState(props.notes)
+
+    const addNote = (event) => {
+        event.preventDefault()
+        console.log('button clicked', event.target)
+    }
+
     return (
         <div>
             <h1>Notes</h1>
             <ul>
-                {notes.map(note => <Note key={note.id} note={note} />)}
+                {
+                    notes.map(
+                    note =>
+                        <Note key={note.id} note={note} />
+                )
+                }
+            </ul>
+            <ul>
+                <form onSubmit={addNote}>
+                    <input />
+                    <button type="submit">Save</button>
+                </form>
             </ul>
         </div>
     )
